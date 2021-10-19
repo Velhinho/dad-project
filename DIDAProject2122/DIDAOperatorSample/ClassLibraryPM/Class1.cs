0adcb0c3847e43c2a30e8cc7b75c218643d194dc
Velhinho <goncalo.velhinho420@gmail.com>
2021-10-19 18:33:48 +0100

    PMGUI and ClassPM

---
+++ DIDAProject2122/DIDAOperatorSample/ClassLibraryPM/Class1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ClassLibraryPM {
+    public class PM_logic {
+       
+        //private ou public???
+        private string schedulerURL;
+        List<workerStruct> WorkersList = new List<workerStruct>();
+        List<StorageStruct> StorageList = new List<StorageStruct>();
+        string populate_file;
+        bool debugMode = false;
+        ClientStruct client = new ClientStruct();
+
+        // le o ficheiro de config
+        // ler nos de storage, workers, etc
+        public void readConfigFile(string file_name) {
+            
+            string path = Path.Combine(Environment.CurrentDirectory, @"files\", file_name);
+            string[] lines = System.IO.File.ReadAllLines(path);
+            string[] words;
+            foreach (string line in lines) {
+
+                words = line.Split(' ');
+                string word = words[0];
+
+                switch (word) {
+                    case "scheduler":
+                        schedulerURL = words[1];
+                        break;
+                    case "worker":
+                        workerStruct auxWorker = new workerStruct();
+                        auxWorker.name = words[1];
+                        auxWorker.url = words[2];
+                        auxWorker.gossipDelay = words[3];
+
+                        WorkersList.Add(auxWorker);
+
+                        break;
+                    case "storage":
+                        StorageStruct auxStorage = new StorageStruct();
+                        auxStorage.name = words[1];
+                        auxStorage.url = words[2];
+                        auxStorage.gossipDelay = words[3];
+
+                        StorageList.Add(auxStorage);
+                        break;
+                    case "populate":
+                        populate_file = words[1];
+                        break;
+                    case "client":
+                        client.app_file = words[2];
+                        client.input = words[1]; 
+                        break;
+                    case "debug":
+                        debugMode = true;
+                        break;
+                    case "status":
+
+                        break;
+                    case "listServer":
+
+                        break;
+                    case "listGlobal":
+
+                        break;
+                    case "crash":
+
+                        break;
+                    case "wait":
+
+                        break;
+                }     
+            }
+        }
+
+        public string listarConfig() {
+
+            string s = "Readed Configs\r\nschedulerURL = " + schedulerURL + "\r\n";
+            //add storage nodes
+            foreach (StorageStruct aux in StorageList) {
+                s = s + "storage node = " + aux.name + "--" + aux.url + "--" + aux.gossipDelay + "\r\n";
+            }
+            //add worker nodes
+            foreach (workerStruct aux in WorkersList) {
+                s = s + "worker node = " + aux.name + "--" + aux.url + "--" + aux.gossipDelay + "\r\n";
+            }
+
+            s = s + "populate file: " + populate_file + "\r\n";
+
+            s = s + "client_input = " + client.input + "--client_app_file = " + client.app_file + "\r\n";
+
+            return s;
+        }
+    }
+
+    //struct de worker
+    struct workerStruct {
+        public string name, url, gossipDelay; 
+    }
+
+    //struct de storage
+    struct StorageStruct {
+        public string name, url, gossipDelay;
+    }
+
+    //struct de client, é apenas uma linha portanto nao existem varias instancias
+    //provavelmente duas varias seria suficiente
+    struct ClientStruct {
+        public string input, app_file;
+    }
+}
---
+++ DIDAProject2122/DIDAOperatorSample/PMGUI/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace PMGUI {
+    partial class FormPM {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.buttonReadConfigFile = new System.Windows.Forms.Button();
+            this.textBoxConfigFileName = new System.Windows.Forms.TextBox();
+            this.textBoxListConfigs = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonReadConfigFile
+            // 
+            this.buttonReadConfigFile.Location = new System.Drawing.Point(13, 45);
+            this.buttonReadConfigFile.Name = "buttonReadConfigFile";
+            this.buttonReadConfigFile.Size = new System.Drawing.Size(151, 27);
+            this.buttonReadConfigFile.TabIndex = 0;
+            this.buttonReadConfigFile.Text = "Read Config File";
+            this.buttonReadConfigFile.UseVisualStyleBackColor = true;
+            this.buttonReadConfigFile.Click += new System.EventHandler(this.buttonReadConfigFile_Click);
+            // 
+            // textBoxConfigFileName
+            // 
+            this.textBoxConfigFileName.Location = new System.Drawing.Point(171, 45);
+            this.textBoxConfigFileName.Name = "textBoxConfigFileName";
+            this.textBoxConfigFileName.Size = new System.Drawing.Size(582, 27);
+            this.textBoxConfigFileName.TabIndex = 1;
+            // 
+            // textBoxListConfigs
+            // 
+            this.textBoxListConfigs.Location = new System.Drawing.Point(13, 78);
+            this.textBoxListConfigs.Multiline = true;
+            this.textBoxListConfigs.Name = "textBoxListConfigs";
+            this.textBoxListConfigs.ReadOnly = true;
+            this.textBoxListConfigs.Size = new System.Drawing.Size(582, 221);
+            this.textBoxListConfigs.TabIndex = 2;
+            // 
+            // FormPM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxListConfigs);
+            this.Controls.Add(this.textBoxConfigFileName);
+            this.Controls.Add(this.buttonReadConfigFile);
+            this.Name = "FormPM";
+            this.Text = "Puppet Master";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonReadConfigFile;
+        private System.Windows.Forms.TextBox textBoxConfigFileName;
+        private System.Windows.Forms.TextBox textBoxListConfigs;
+    }
+}
+
