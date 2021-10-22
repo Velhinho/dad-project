6d55bbc1f35f555924daf53bbc1cb60ca1ca3edf
Genio-D <diogodias001@gmail.com>
2021-10-22 18:49:45 +0100

    added proto base for worker

--- DIDAProject2122/DIDAOperatorSample/Worker/Worker.cs
+++ DIDAProject2122/DIDAOperatorSample/Worker/Worker.cs
@@ -1,13 +1,35 @@
 ﻿using System;
+using Grpc.Core;
 
 namespace Worker {
+
+    class WorkerServerService : DIDAWorkerServerService.DIDAWorkerServerServiceBase
+    {
+        string worker_id;
+        public WorkerServerService(string id)
+        {
+            worker_id = id;
+        }
+    }
+
     class Worker {
         static void Main(string[] args) {
 
             foreach (var item in args) {
                 Console.WriteLine(item.ToString());
             }
-            Console.ReadLine();
+            string id = args[1];
+            Uri uri = new Uri(args[2]);
+            int port = uri.Port;
+            Server server = new Server
+            {
+                Services = { DIDAWorkerServerService.BindService(new WorkerServerService(id)) },
+                Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+            Console.WriteLine("Server " + id + " started on port " + port);
+            Console.ReadKey();
+            server.ShutdownAsync().Wait();
 
         }
     }
